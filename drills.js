e4891a595a45b3895e35c79cff1ff0cd9a95cfcb
Shawn Collette <shawn.collette@gmail.com>
2019-11-13 12:43:29 -0700

    #3 complete

--- drills.js
+++ drills.js
@@ -28,3 +28,119 @@
 //filter 10^-1
 //filter 10^-2
 //filter 10^-3
+
+
+//3.)  Create a function to query a library.  You have title and dewey decimal.
+
+const BST = require('./BST');
+
+const library = [
+  { author: 'Cowlishaw, Mike', dewey: '005.133', title: 'The REXX Language' },
+  { author: 'Sams', dewey: '005.133', title: 'Teach Yourself C++ In 21 Days' },
+  { author: 'Stroustrup., Bjarne', dewey: '005.133', title: 'The C++ Programming Language' },
+  { author: 'Crockford, Douglas', dewey: '005.2762', title: 'JavaScript: The Good Parts' },
+  { author: 'Flanagan, David', dewey: '005.2762', title: 'JavaScript: The Definitive Guide' },
+  { author: 'Schmidt, Meinhard', dewey: '005.44684', title: 'Windows Vista for Dummies' },
+  { author: 'Zondervan', dewey: '220.52081', title: 'NIV Study Bible' },
+  { author:'Humphries, Russell, Dr.', dewey: '231.7652', title: 'Starlight and Time' },
+  { author: 'Jane, Frederick Thomas', dewey: '623.82509051', title: 'Jane\'s Fighting Ships' },
+  { author: 'Norris, Chuck', dewey: '796.8092', title: 'The Official Chuck Norris Fact Book' }
+];
+
+function trim(dewey, library){
+  let trimmed = [];
+  for(let i = 0; i < library.length; i++){
+    if(library[i]['dewey'] === dewey){
+      trimmed.push(library[i])
+    }
+  }
+  return trimmed;
+}
+
+function findBook(title, library, start = 0, end = library[library.length-1]){
+  let index = Math.floor(library.length/2)
+  if(title === library[index]['title']){
+    return library[index]
+  }
+  if(start > end){
+    return -1
+  }
+
+  findBook(title, library, start, index--)
+  findBook(title, library, index++, end)
+}
+
+
+function main(){
+
+  console.log(findBook('JavaScript: The Definitive Guide', trim('005.2762', library)))
+
+}
+
+main()
+
+
+/*
+INTERVIEW QUESTION OF THE DAY:
+Given a postorder traversal of a BST, construct the BST.
+(don't worry about runtime or other optimization)
+
+INPUT: 8, 12, 10, 16, 25, 20, 15 
+(Note:  this is the output from running the dsfPostOder() below...)
+
+OUTPUT: ...
+
+              15
+            /    \
+          10      20
+         /  \    /  \
+        8   12  16   25
+
+
+
+SOLUTION:
+
+ class BST{
+     constructor(key,parent){
+         this.key = key;
+         this.parent = parent;
+         this.left = null;
+         this.right = null;
+     }
+ }
+
+ function postToBST(arr, start=0, end=arr.length-1){
+    if(start > end){
+        return;
+    }
+
+    let bst = new BST(arr[end])
+    let i;
+    for(i=end-1;i>start;i--){
+        if(arr[i]<arr[end]){
+            break;
+        }
+    }
+    bst.left = postToBST(arr,start,i);
+    bst.right = postToBST(arr,i+1,end-1);
+    return bst;
+ }
+
+
+
+
+POST ORDER FUNCTION:
+
+dsfPostOrder() {
+    if (this.left) {
+        this.left.dsfPostOrder();
+    }
+    if (this.right) {
+        this.right.dsfPostOrder();
+    }
+    console.log(this.key);
+}
+
+*/
+
+
